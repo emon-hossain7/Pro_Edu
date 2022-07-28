80437f03c1f9130e69cb02a6746cb339d192340e
emon-hossain7 <emon.hossain.wd@gmail.com>
2022-07-28 10:19:56 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    './index.html',
+  ],
+
+
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
