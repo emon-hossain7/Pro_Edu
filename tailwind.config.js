8b2d1010d314f43102775712a396d88dc5347a6c
emon-hossain7 <emon.hossain.wd@gmail.com>
2022-07-29 09:55:22 +0600

    font add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",
-    './index.html',
-  ],
-
-
-  content: [
     './components/**/*.{html,js}',
     './pages/**/*.{html,js}',
     './index.html',
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', sans-serif],
+      },
+    },
   },
   plugins: [],
 }
